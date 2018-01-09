6cf92183a8da8fdc0aa97353968182850bac1d3c
huijie <huijie@l-021-cyl00276.geely.auto>
2018-01-09 15:44:44 +0800

    Update -> 1.更新模板

---
+++ Cocoa Touch Class.xctemplate/NSObjectObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,42 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Life Cycle
+
+#pragma mark - Event Response
+
+#pragma mark - --Notification Event Response
+
+#pragma mark - --Button Event Response
+
+#pragma mark - --Gesture Event Response
+
+#pragma mark - --Timer Event Response
+
+#pragma mark - System Delegate
+
+#pragma mark - Custom Delegate
+
+#pragma mark - Public Function
+
+#pragma mark - Private Function
+
+#pragma mark - Getter and Setter
+#pragma mark - --Setter
+
+#pragma mark - --Getter
+
+@end
